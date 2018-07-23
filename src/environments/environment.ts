60976bf50def4bedec5ed7225dbab6ce73b839f7
onewiseburdman <onewiseburdman@gmail.com>
2018-07-23 12:41:49 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,5 @@
 
 export const environment = {
   production: false,
-  firebaseConfig:{
-    apiKey: "AIzaSyCufSaVQ5gRAbik6LHj2zL4Clq4UR-a-0c",
-    authDomain: "ballotboxcompany-2cf91.firebaseapp.com",
-    databaseURL: "https://ballotboxcompany-2cf91.firebaseio.com",
-    projectId: "ballotboxcompany-2cf91",
-    storageBucket: "ballotboxcompany-2cf91.appspot.com",
-    messagingSenderId: "654180350642"
-  },
-  algolia: {
-    appId: 'QBC5HI4F6R',
-    apiKey: 'ac031b6f39e98d93194cd48099fd6d59'
-  },
-  functionsUrl: 'https://us-central1-ballotboxcompany-2cf91.cloudfunctions.net/recurringPaymentWebhook',
-  stripePublishable:'pk_test_rwy6PWuaAjbZzkuoAitpNZ5Y'
+ 
 };
