2d30cb90c51ca095d678e433a76683cb93d3f39e
Dad <dad@Chriss-MacBook-Pro.local>
2018-09-13 09:27:35 -0400

    new commit

--- src/app/ui/dynamicbackground/dynamicbackground.component.ts
+++ src/app/ui/dynamicbackground/dynamicbackground.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
+
+
 
 @Component({
   selector: 'dynamicbackground',
-  template: `<ng-container *ngFor="let style of styles" >
-  <div [style.background-image]="'url(' + style.background + ')'"></div>
+  template: `<ng-container *ngFor="let style of styles">
+  <div [style.background-image]="'url(' + style.background + ')'"  *ngIf="style.background!=undefined"></div>
   </ng-container> `
 })
 export class dynamicbackgroundComponent implements OnInit {
   @Input() styles: string;
-    
+
     ngOnInit() {
 
     }
